4b1d215b1272048f30e57434dff204f12a507135
ZIMMM <turnerro@tcd.ie>
2017-10-20 18:11:05 +0100

    Add files via upload

---
+++ LowestCommonAncestorTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LowestCommonAncestorTest {
+	
+	@Test
+	public void NodeGetDataTest() {
+		Node node = new Node(0);
+		assertEquals(0, node.getData());
+	}
+	
+	
+	
+	
+
+}
