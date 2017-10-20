3f82f591d21f7bdf7d11ffd8056520cc9caf6c87
ZIMMM <turnerro@tcd.ie>
2017-10-20 18:15:11 +0100

    Add files via upload

--- LowestCommonAncestorTest.java
+++ LowestCommonAncestorTest.java
@@ -10,7 +10,29 @@ public class LowestCommonAncestorTest {
 		assertEquals(0, node.getData());
 	}
 	
+	@Test
+	public void NodeSetLeftGetLeftTest() {
+		Node node = new Node(0);
+		Node left = new Node(1);
+		node.setLeft(left);
+		assertSame(left, node.getLeft());
+	}
 	
+	@Test
+	public void NodeSetRightGetRightTest() {
+		Node node = new Node(0);
+		Node right = new Node(1);
+		node.setRight(right);
+		assertSame(right, node.getRight());
+	}
+
+	@Test
+	public void BinaryTreeIsEmptyTestAndInsertTest() {
+		BinaryT BinaryT = new BinaryT();
+		assertTrue(BinaryT.isEmpty());
+		BinaryT.insert(0);
+		assertFalse(BinaryT.isEmpty());
+	}
 	
 	
 
