58196b7f24f1303d75128aef2c5c73014ed127e8
ZIMMM <turnerro@tcd.ie>
2017-11-01 17:32:31 +0000

    Add files via upload

--- GraphLinkedList.java
+++ GraphLinkedList.java
@@ -1,5 +1,75 @@
 package com.sanfoundry.hardgraph;
 
-public class GraphLinkedList {
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
+class GraphLinkedList
+{
+    private Map<Integer, List<Integer>> adjacencyList;
+ 
+    public GraphLinkedList(int v)
+    {
+        adjacencyList = new HashMap<Integer, List<Integer>>();
+        for (int i = 1; i <= v; i++)
+            adjacencyList.put(i, new LinkedList<Integer>());
+    }
+ 
+    public void setEdge(int from, int to)
+    {
+        if (to > adjacencyList.size() || from > adjacencyList.size())
+            System.out.println("The vertices does not exists");
+    
+        List<Integer> dls = adjacencyList.get(from);
+        dls.add(to);
+    }
+ 
+    public List<Integer> getEdge(int to)
+    {
+        if (to > adjacencyList.size())
+        {
+            System.out.println("The vertices does not exists");
+            return null;
+        }
+        return adjacencyList.get(to);
+    }
+ 
+    public boolean checkDAG()
+    {
+        Integer count = 0;
+        Iterator<Integer> iteratorI = this.adjacencyList.keySet().iterator();
+        Integer size = this.adjacencyList.size() - 1;
+        while (iteratorI.hasNext())
+        {
+            Integer i = iteratorI.next();
+            List<Integer> adjList = this.adjacencyList.get(i);
+            if (count == size)
+            {
+                return true;
+            }
+            if (adjList.size() == 0)
+            {
+                count++;
+                System.out.println("Target Node - " + i);
+                Iterator<Integer> iteratorJ = this.adjacencyList.keySet()
+                        .iterator();
+                while (iteratorJ.hasNext())
+                {
+                    Integer j = iteratorJ.next();
+                    List<Integer> li = this.adjacencyList.get(j);
+                    if (li.contains(i))
+                    {
+                        li.remove(i);
+                        System.out.println("Deleting edge between target node "
+                                + i + " - " + j + " ");
+                    }
+                }
+                this.adjacencyList.remove(i);
+                iteratorI = this.adjacencyList.keySet().iterator();
+            }
+        }
+        return false;
+    }
 }
