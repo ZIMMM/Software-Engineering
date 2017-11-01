7bdcb559e466e1ffacbb488ce28c32b8192462f4
ZIMMM <turnerro@tcd.ie>
2017-11-01 21:00:50 +0000

    Add files via upload

--- LowestCommonAncestorDag.java
+++ LowestCommonAncestorDag.java
@@ -1,5 +1,4 @@
 
-
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -7,11 +6,8 @@ import java.util.Set;
 import java.util.Stack;
 
 public class LowestCommonAncestorDag {
-	
-	 Node<Integer> root;
-	    Set<Node<Integer>> allNodes;
-	    
-	
+
+	Set<Node<Integer>> allNodes = new HashSet<>();
 
 	public class Node<T> {
 		T data;
@@ -22,30 +18,61 @@ public class LowestCommonAncestorDag {
 			this.adList = new HashSet<>();
 		}
 
+		public Node<Integer> createNode(Integer data) {
+			Node<Integer> node = new Node<Integer>(data);
+			allNodes.add(node);
+			return node;
+		}
+
 		public void addEdge(Node<T> node) {
 			adList.add(node);
 		}
-	
-			
-	
 
+		public Node<Integer> LCA(Node<Integer> anode, Node<Integer> n1, Node<Integer> n2) {
+			List<Node<Integer>> list0 = DFS(anode, n1, new ArrayList<>(), new Stack<>());
+			List<Node<Integer>> list1 = DFS(anode, n2, new ArrayList<>(), new Stack<>());
+
+			List<Node<Integer>> min;
+			List<Node<Integer>> max;
+
+			if (list1.size() <= list1.size()) {
+				min = list0;
+				max = list1;
+			} 
+			else {
+				min = list1;
+				max = list0;
+			}
+			if (max.contains(n1)) {
+				return (n1);
+			}
+			else if (max.contains(n2)) {
+				return (n2);
+			} 
+			
+			else {
+				for (int i = max.size() - 1; i >= 0; i--) {
+					if (min.contains(max.get(i))) {
+						return max.get(i);
+					}
+				}
+			}
+			return null;
+		}
 	}
-	
-	 private List<Node<Integer>> depthFirstSearch(Node<Integer> node, Node<Integer> targetNode, List<Node<Integer>> list, Stack<Node<Integer>> stack) 
-	    {
-	        stack.push(node);
-	        for (Node<Integer> n : node.adList) 
-	        {
-	            if (n.equals(targetNode)) 
-	            {
-	                list.addAll(stack);
-	                return list;
-	            }
-	            depthFirstSearch(n, targetNode, list, stack);
-	        }
-	        stack.pop();
-	        return list;
-	    }
-	
-	
+
+	private List<Node<Integer>> DFS(Node<Integer> node, Node<Integer> targetNode, List<Node<Integer>> list,
+			Stack<Node<Integer>> stack) {
+		stack.push(node);
+		for (Node<Integer> n : node.adList) {
+			if (n.equals(targetNode)) {
+				list.addAll(stack);
+				return list;
+			}
+			DFS(n, targetNode, list, stack);
+		}
+		stack.pop();
+		return list;
+	}
+
 }
